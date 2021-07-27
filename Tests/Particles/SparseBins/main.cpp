80a330eac363470b1a848dbe42b6c15944525514
Andrew Myers <atmyers2@gmail.com>
2021-07-27 15:29:19 -0700

    update sparse bins interface and test.

--- Tests/Particles/SparseBins/main.cpp
+++ Tests/Particles/SparseBins/main.cpp
@@ -1,80 +1,79 @@
 #include <AMReX.H>
-#include <AMReX_MultiFab.H>
+#include <AMReX_Vector.H>
 #include <AMReX_ParmParse.H>
 #include <AMReX_SparseBins.H>
 
 using namespace amrex;
 
-struct TestParams
+void checkAnswer (const amrex::SparseBins<int>& bins)
 {
-    IntVect size;
-    int max_grid_size;
-    int is_periodic;
-    int nlevs;
-};
+    BL_PROFILE("checkAnswer");
+    const auto perm = bins.permutationPtr();
+    const auto bins_ptr = bins.getNonZeroBinsPtr();
+    const auto offsets = bins.offsetsPtr();
 
-void get_test_params(TestParams& params, const std::string& prefix)
-{
-    ParmParse pp(prefix);
-    pp.get("size", params.size);
-    pp.get("max_grid_size", params.max_grid_size);
-    pp.get("is_periodic", params.is_periodic);
-    pp.get("nlevs", params.nlevs);
-}
-
-void testIntersection()
-{
-    TestParams params;
-    get_test_params(params, "intersect");
-
-    int is_per[BL_SPACEDIM];
-    for (int i = 0; i < BL_SPACEDIM; i++)
-        is_per[i] = params.is_periodic;
-
-    Vector<IntVect> rr(params.nlevs-1);
-    for (int lev = 1; lev < params.nlevs; lev++)
-        rr[lev-1] = IntVect(AMREX_D_DECL(2,2,2));
-
-    RealBox real_box;
-    for (int n = 0; n < AMREX_SPACEDIM; n++)
+#ifdef AMREX_USE_OMP
+#pragma omp parallel for
+#endif
+    for (int i = 0; i < bins.numItems()-1; ++i)
     {
-        real_box.setLo(n, 0.0);
-        real_box.setHi(n, 1.0);
+        AMREX_ALWAYS_ASSERT(bins_ptr[perm[i]] <= bins_ptr[perm[i+1]]);
     }
 
-    IntVect domain_lo(AMREX_D_DECL(0 , 0, 0));
-    IntVect domain_hi(AMREX_D_DECL(params.size[0]-1, params.size[1]-1, params.size[2]-1));
-    const Box base_domain(domain_lo, domain_hi);
-
-    Vector<Geometry> geom(params.nlevs);
-    geom[0].define(base_domain, &real_box, CoordSys::cartesian, is_per);
-    for (int lev = 1; lev < params.nlevs; lev++) {
-        geom[lev].define(amrex::refine(geom[lev-1].Domain(), rr[lev-1]),
-                         &real_box, CoordSys::cartesian, is_per);
-    }
-
-    Vector<BoxArray> ba(params.nlevs);
-    IntVect lo = IntVect(AMREX_D_DECL(0, 0, 0));
-    IntVect size = params.size;
-    for (int lev = 0; lev < params.nlevs; ++lev)
-    {
-        ba[lev].define(Box(lo, lo+params.size-1));
-        ba[lev].maxSize(params.max_grid_size);
-        lo += size/2;
-        size *= 2;
-    }
-
-    {
-//        auto num_boxes = ba[0].size();
-
+#ifdef AMREX_USE_OMP
+#pragma omp parallel for
+#endif
+    for (int i = 0; i < bins.numBins(); ++i) {
+        auto start = offsets[i  ];
+        auto stop  = offsets[i+1];
+        if (start == stop) continue;
+        for (auto j = start+1; j < stop; ++j)
+        {
+            AMREX_ALWAYS_ASSERT(bins_ptr[perm[start]] == bins_ptr[perm[j]]);
+        }
     }
 }
 
-int main(int argc, char* argv[])
+void test (int nbins, const amrex::Vector<int>& items)
+{
+    amrex::SparseBins<int> bins;
+    bins.build(items.size(), items.data(), nbins, [=] AMREX_GPU_DEVICE (int j) noexcept -> unsigned int { return j ; });
+
+    checkAnswer(bins);
+}
+
+void initData (int nbins, amrex::Vector<int>& items)
+{
+    BL_PROFILE("init");
+
+    const int nitems = items.size();
+
+#ifdef AMREX_USE_OMP
+#pragma omp parallel for
+#endif
+    for (int i = 0; i < nitems; ++i) { items[i] = amrex::Random_int(nbins); }
+}
+
+void testSparseBins ()
+{
+    int nitems;
+    int nbins;
+
+    ParmParse pp;
+    pp.get("nitems", nitems);
+    pp.get("nbins" , nbins);
+
+    amrex::Vector<int> items(nitems);
+    initData(nbins, items);
+
+    test(nbins, items);
+}
+
+int main (int argc, char* argv[])
 {
     amrex::Initialize(argc,argv);
 
-    testIntersection();
+    testSparseBins();
 
     amrex::Finalize();
 }
